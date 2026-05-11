44c1016cd774d449838477d82d5597b413870154
nvthiets <154614288+nvthiets@users.noreply.github.com>
2026-05-11 14:08:38 +0700

    change the name

--- custom.ts
+++ custom.ts
@@ -2,7 +2,7 @@
  * Thư viện Robot Lõi
  */
 //% weight=100 color=#0fbc11 icon="\uf11b" block="Robot Lõi"
-namespace RobotCore {
+namespace Expansion_Microbit {
     /**
      * Điều khiển góc quay của động cơ Servo
      * @param pin Chân cắm servo, eg: AnalogPin.P1

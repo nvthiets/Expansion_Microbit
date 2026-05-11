c4bda9b97b2408822f41c6fbab6bb7b3ce1c2c5a
nvthiets <154614288+nvthiets@users.noreply.github.com>
2026-05-11 09:52:14 +0700

    Init core

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
-})
+namespace RobotCore {
+
+    /**
+     * Khối lệnh test kiểm tra hệ thống
+     */
+    //% block="kiểm tra kết nối"
+    export function systemCheck(): void {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+}

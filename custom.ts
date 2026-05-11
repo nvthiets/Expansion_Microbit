88c306b376ab86d894afdd95ea63408bf9b2329c
nvthiets <154614288+nvthiets@users.noreply.github.com>
2026-05-11 11:43:54 +0700

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,64 +1,46 @@
+/**
+ * Thư viện Robot Lõi
+ */
 //% weight=100 color=#0fbc11 icon="\uf11b" block="Robot Lõi"
 namespace RobotCore {
     /**
-     * Quay Servo
+     * Quay Servo
+     * @param pin Chân cắm servo, eg: AnalogPin.P1
+     * @param angle Góc quay 0-180, eg: 90
      */
-    //% block="Quay servo chân $pin góc $angle độ"
-    //% angle.min=0 angle.max=180
+    //% block="Quay servo chân %pin góc %angle độ"
+    //% angle.min=0 angle.max=180
     export function servoControl(pin: AnalogPin, angle: number): void {
         pins.servoWritePin(pin, angle)
     }
 }
-//% weight=90 color=#e74c3c icon="feed" block="RobotIR"
+
+/**
+ * Thư viện Robot IR
+ */
+//% weight=90 color=#e74c3c icon="\uf012" block="RobotIR"
 namespace RobotIR {
     let irCode = -1;
     let dataRaw = 0;
     let bitCount = 0;
 
-    /**
-     * Danh sách các nút bấm trên Remote 17 phím
-     */
     export enum Button {
-        //% block="Lên"
+        //% block="Lên"
         Up = 0x62,
-        //% block="Xuống"
+        //% block="Xuống"
         Down = 0xA8,
-        //% block="Trái"
+        //% block="Trái"
         Left = 0x22,
-        //% block="Phải"
+        //% block="Phải"
         Right = 0xC2,
-        //% block="OK"
-        OK = 0x02,
-        //% block="1"
-        Num1 = 0x68,
-        //% block="2"
-        Num2 = 0x98,
-        //% block="3"
-        Num3 = 0xB0,
-        //% block="4"
-        Num4 = 0x30,
-        //% block="5"
-        Num5 = 0x18,
-        //% block="6"
-        Num6 = 0x7A,
-        //% block="7"
-        Num10 = 0x10,
-        //% block="8"
-        Num8 = 0x38,
-        //% block="9"
-        Num9 = 0x5A,
-        //% block="0"
-        Num0 = 0x4A,
-        //% block="*"
-        Star = 0x42,
-        //% block="#"
-        Hash = 0x52
+        //% block="OK"
+        OK = 0x02
     }
 
     /**
-     * Khởi tạo mắt thu IR tại chân P16
+     * Khởi tạo mắt thu IR
      */
-    //% block="Khởi tạo mắt thu IR tại chân %pin"
+    //% block="Khởi tạo mắt thu IR tại chân %pin"
     export function initIR(pin: DigitalPin): void {
         pins.setPull(pin, PinPullMode.PullUp);
         pins.onPulsed(pin, PulseValue.High, function () {
@@ -78,9 +60,9 @@ namespace RobotIR {
     }
 
     /**
-     * Kiểm tra nút bấm
+     * Kiểm tra nút bấm
      */
-    //% block="Nút %btn được bấm?"
+    //% block="Nút %btn được bấm?"
     export function isButtonPressed(btn: Button): boolean {
         if (irCode === btn) {
             irCode = -1;
